a601fef0dcc469611a6ac91b7979d1da22fbe9fe
lin769558-gif <lin769558@gmail.com>
2025-08-30 09:37:47 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -6,21 +6,21 @@ const CONFIG = {
   PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
 
   // 英雄区块导航
-  PROXIO_HERO_ENABLE: true, // 开启英雄区
+  PROXIO_HERO_ENABLE: false, // 开启英雄区
   PROXIO_HERO_TITLE_1: '欢迎来到我的作品站', // 英雄区文字
   PROXIO_HERO_TITLE_2: '工业设计-服务设计-体验设计', // 英雄区文字
   // 英雄区两个按钮，如果TEXT
   留空则隐藏按钮
-  PROXIO_HERO_BUTTON_1_TEXT: '', // 英雄区按钮
+  PROXIO_HERO_BUTTON_1_TEXT: '1', // 英雄区按钮
   PROXIO_HERO_BUTTON_1_URL:
     'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_TEXT: '', // 英雄区按钮
+  PROXIO_HERO_BUTTON_2_TEXT: '1', // 英雄区按钮
   PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github-mark.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
-  PROXIO_HERO_BANNER_IFRAME_URL: '', // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
+  PROXIO_HERO_BANNER_IMAGE: '1', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
+  PROXIO_HERO_BANNER_IFRAME_URL: '1', // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
 
   // 文章区块
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
