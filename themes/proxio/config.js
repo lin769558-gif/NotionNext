b8903bcfa40ac01bd594acc2b3db54b29cf0b669
lin769558-gif <lin769558@gmail.com>
2025-08-30 09:38:51 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -2,8 +2,8 @@
  * 另一个落地页主题
  */
 const CONFIG = {
-  PROXIO_WELCOME_COVER_ENABLE: false, //是否显示页面进入的欢迎文字
-  PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
+  PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
+  PROXIO_WELCOME_TEXT: '欢迎！点击进入我的作品站', // 欢迎文字，留空则不启用
 
   // 英雄区块导航
   PROXIO_HERO_ENABLE: false, // 开启英雄区
